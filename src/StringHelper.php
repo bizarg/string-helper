c9fdc8dd56bc28b0de4e3189caa476cdd21bc7e1
Chyzhevskiy <aleksandr.chyzhevskiy@it-devgroup.com>
2023-09-11 14:16:01 +0300

    update

--- src/StringHelper.php
+++ src/StringHelper.php
@@ -2,16 +2,8 @@
 
 namespace Bizarg\StringHelper;
 
-/**
- * Class StringHelper
- * @package Bizarg\StringHelper
- */
 class StringHelper
 {
-    /**
-     * @param string $input
-     * @return array
-     */
     public static function match(string $input): array
     {
         preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
@@ -19,29 +11,16 @@ class StringHelper
         return $matches[0];
     }
 
-    /**
-     * @param string $input
-     * @return string
-     */
     public static function toUnderscore(string $input): string
     {
         return self::toGlue($input, '_');
     }
 
-    /**
-     * @param string $input
-     * @return string
-     */
     public static function toHyphen(string $input): string
     {
         return self::toGlue($input, '-');
     }
 
-    /**
-     * @param string $input
-     * @param string $glue
-     * @return string
-     */
     public static function toGlue(string $input, string $glue): string
     {
         $ret = self::match($input);
@@ -53,10 +32,6 @@ class StringHelper
         return implode($glue, $ret);
     }
 
-    /**
-     * @param string $input
-     * @return string
-     */
     public static function camelCase(string $input): string
     {
         $ret = self::match($input);
@@ -68,10 +43,6 @@ class StringHelper
         return implode($ret);
     }
 
-    /**
-     * @param string $input
-     * @return string
-     */
     public static function upperCaseCamelCase(string $input): string
     {
         return implode(array_map('ucfirst', self::match($input)));
